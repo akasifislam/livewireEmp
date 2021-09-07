c9a2bc6d83d574af171be60775caf7fdeeb4c562
Muhammad Asif Ul Islam <aseasifislam@gmail.com>
2021-09-07 22:37:46 +0600

    Feat: country component

---
+++ app/Http/Livewire/CountryComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class CountryComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.country-component');
+    }
+}
